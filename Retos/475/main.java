70037783d50a1b44f0a49a63493f2988633820d4
Richardo G <noctismaiestatem@gmail.com>
2019-09-30 21:16:21 +0200

    JUSTINCASE

--- Retos/475/main.java
+++ Retos/475/main.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class main{
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         char[] ID = "stanle".toCharArray();
 
